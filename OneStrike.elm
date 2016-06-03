d9862e476b775c77938eac4175fe573600d7b21f
Sergey Kopyrin <s.quoddam@gmail.com>
2016-06-03 08:54:38 +0300

    Make basic movement

--- OneStrike.elm
+++ OneStrike.elm
@@ -7,8 +7,10 @@ import Collage exposing (..)
 import Element
 import Time
 import Color exposing (red)
+import Debug
 
 import Mouse
+import Keyboard
 
 main = Html.App.program({
   view = view,
@@ -19,23 +21,94 @@ main = Html.App.program({
 
 -- MODEL
 
+type alias Player =
+  { x : Int
+  , y : Int
+  , s : Int
+  }
+
 type alias Model =
-  { mouse: Mouse.Position
+  { player : Player
+  , mouse: Mouse.Position
+  , keysDown : List Keyboard.KeyCode
   }
 
 init : (Model, Cmd Msg)
-init = (Model {x = 0, y = 0}, Cmd.none)
+init = (Model  (Player 0 0 5) {x = 0, y = 0} [], Cmd.none)
 
 -- UPDATE
 
-type Msg =
-  MouseMove Mouse.Position
+type Msg
+  = MouseMove Mouse.Position
+  | KeyDown Keyboard.KeyCode
+  | KeyUp Keyboard.KeyCode
+  | Tick Time.Time
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     MouseMove pos ->
-      ({model | mouse = pos}, Cmd.none)
+      {model | mouse = pos} ! []
+
+    KeyDown key ->
+      {model | keysDown = addKey key model.keysDown} ! []
+
+    KeyUp key ->
+      {model | keysDown = removeKey key model.keysDown} ! []
+
+    Tick _->
+      applyChanges model ! []
+
+
+addKey : Keyboard.KeyCode -> List Keyboard.KeyCode -> List Keyboard.KeyCode
+addKey key keysDown =
+  if List.member key keysDown
+    then keysDown
+    else key :: keysDown
+
+removeKey : Keyboard.KeyCode -> List Keyboard.KeyCode -> List Keyboard.KeyCode
+removeKey key keysDown =
+  List.filter (\k -> k /= key) keysDown
+
+applyChanges : Model -> Model
+applyChanges model =
+  let
+    player = model.player
+    mOfKD x = List.member x model.keysDown
+  in
+    { model
+    | player =
+      { player
+      | y =
+          List.foldl
+            (\key y ->
+              case key of
+                87 ->
+                  y + model.player.s
+
+                83 ->
+                  y - model.player.s
+
+                _->
+                  y
+            ) player.y model.keysDown
+      , x =
+          List.foldl
+            (\key x ->
+              case key of
+                68 ->
+                  x + model.player.s
+
+                65 ->
+                  x - model.player.s
+
+                _->
+                  x
+            ) player.x model.keysDown
+      }
+    }
+
+
 
 -- VIEW
 
@@ -49,9 +122,17 @@ view model =
     Element.toHtml
       <| collage 500 500
       [
-        playerView <| relativeMousePosition model.mouse
+      move (
+      toFloat  model.player.x,
+      toFloat  model.player.y
+      )
+      <| playerView <| relativeMousePosition model.mouse
       ]
   ]
+
+-- movePlayer : List Int -> Form -> Form
+-- movePlayer (x,y) player =
+--
 
 playerView : Mouse.Position -> Form
 playerView pos =
@@ -81,4 +162,9 @@ relativeMousePosition {x, y} =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Mouse.moves MouseMove
+  Sub.batch
+  [ Mouse.moves MouseMove
+  , Keyboard.downs KeyDown
+  , Keyboard.ups KeyUp
+  , Time.every (20 * Time.millisecond) Tick
+  ]
